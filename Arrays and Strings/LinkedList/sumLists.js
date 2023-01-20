22df2e5d60270288079c5ab440f919d46d952001
MikeAriyo <ariyoadekola2@gmail.com>
2023-01-20 22:18:53 +0100

    lists sum

---
+++ Arrays and Strings/LinkedList/sumLists.js
@@ -0,0 +1,24 @@
+// An algorithm to add two numbers and returns the sum as a linked list
+
+function addLists(l1, l2) {
+  let dummyHead = new ListNode(0);
+  let current = dummyHead;
+  let carry = 0;
+
+  while (l1 || l2) {
+    let x = l1 ? l1.val : 0;
+    let y = l2 ? l2.val : 0;
+    let sum = carry + x + y;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  if (carry > 0) {
+    current.next = new ListNode(carry);
+  }
+
+  return dummyHead.next;
+}
