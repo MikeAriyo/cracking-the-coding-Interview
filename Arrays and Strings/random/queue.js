9ce36561a357f28452fef2f2a5165b240807350b
MikeAriyo <ariyoadekola2@gmail.com>
2023-01-30 20:58:05 +0100

    started work on queue

---
+++ Arrays and Strings/random/queue.js
@@ -0,0 +1,19 @@
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(item) {
+    this.stack1.push(item);
+  }
+
+  dequeue() {
+    if (this.stack2.length === 0) {
+      while (this.stack1.length > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2.pop();
+  }
+}
